616c0afc01792724363aca473927a57508d9e3ac
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-06-30 16:28:06 +0300

    beans configurated

---
+++ user-microservice/user-microservice/src/main/java/com/iknow/usermicroservice/config/BeanConfig.java
@@ -0,0 +1,19 @@
+package com.iknow.usermicroservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
