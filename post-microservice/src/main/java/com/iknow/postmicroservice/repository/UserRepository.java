198b9762ff6454d60e89c9a035385c896f22e5fb
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-04 17:00:32 +0300

    user repository updated

--- post-microservice/src/main/java/com/iknow/postmicroservice/repository/UserRepository.java
+++ post-microservice/src/main/java/com/iknow/postmicroservice/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.iknow.postmicroservice.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User,String> {
 
+    Optional<User> findUserByUsername(String username);
+    Optional<User> findUserById(String id);
 }
