dc7e076c7b069d2ce9681a456bad29b586a77f36
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-04 10:45:34 +0300

    files moved

---
+++ post-microservice/src/main/java/com/iknow/postmicroservice/PostMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.iknow.postmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostMicroserviceApplication.class, args);
+	}
+
+}
---
+++ post-microservice/src/test/java/com/iknow/postmicroservice/PostMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iknow.postmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
