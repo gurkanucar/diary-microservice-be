0d98ce781d22fe4c6132dd829504fb5ac9a5e4d0
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-04 11:19:14 +0300

    user model created

---
+++ post-microservice/src/main/java/com/iknow/postmicroservice/model/User.java
@@ -0,0 +1,27 @@
+package com.iknow.postmicroservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class User {
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String username;
+    private String profileImageUrl;
+    private String name;
+    private String surname;
+
+}
