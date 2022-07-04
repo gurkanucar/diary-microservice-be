da5ca98c348a46ef911afc002cbb19afc9915da5
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-04 16:59:32 +0300

    comment model updated

--- post-microservice/src/main/java/com/iknow/postmicroservice/model/Comment.java
+++ post-microservice/src/main/java/com/iknow/postmicroservice/model/Comment.java
@@ -1,4 +1,20 @@
 package com.iknow.postmicroservice.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Comment {
+
+    private String id;
+    private User user;
+    private String content;
+
+
 }
