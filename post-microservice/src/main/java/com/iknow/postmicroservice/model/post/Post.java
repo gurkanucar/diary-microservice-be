d5ca76540ed53e946d14739e948ec65142ff0e46
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-04 11:19:48 +0300

    post model created

---
+++ post-microservice/src/main/java/com/iknow/postmicroservice/model/post/Post.java
@@ -0,0 +1,29 @@
+package com.iknow.postmicroservice.model.post;
+
+import com.iknow.postmicroservice.model.Comment;
+import com.iknow.postmicroservice.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class Post {
+    @Id
+    private String id;
+    private String content;
+    private User user;
+    private List<User> likedUsers;
+    private List<Comment> comments;
+    private LocalDateTime created;
+}
