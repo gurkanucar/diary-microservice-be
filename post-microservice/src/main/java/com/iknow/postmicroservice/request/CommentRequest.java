fde3f6a26d89e9097b6cc40e6d45a24a4d313e6e
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-05 11:07:10 +0300

    comment request updated

--- post-microservice/src/main/java/com/iknow/postmicroservice/request/CommentRequest.java
+++ post-microservice/src/main/java/com/iknow/postmicroservice/request/CommentRequest.java
@@ -3,10 +3,14 @@ package com.iknow.postmicroservice.request;
 import com.iknow.postmicroservice.model.Comment;
 import com.iknow.postmicroservice.model.post.Post;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CommentRequest {
 
     private Post post;
