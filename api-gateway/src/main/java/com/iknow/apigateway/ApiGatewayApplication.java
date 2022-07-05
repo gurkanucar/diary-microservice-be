cb4b5f2773664ca419d9822ead16d9a82fbb906d
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-05 17:48:14 +0300

    feign client configurated

--- api-gateway/src/main/java/com/iknow/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/iknow/apigateway/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.iknow.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ApiGatewayApplication {
 
