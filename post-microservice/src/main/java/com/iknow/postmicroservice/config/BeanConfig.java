97cd2ef09b971d5ee07c09897aa17e5318826e23
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-05 11:25:31 +0300

    security configuration

---
+++ post-microservice/src/main/java/com/iknow/postmicroservice/config/BeanConfig.java
@@ -0,0 +1,15 @@
+package com.iknow.postmicroservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
