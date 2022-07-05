f6d5622c3287f1107060f55ebbc72a9dc2846fd4
gurkan ucar <gurkanucar@trakya.edu.tr>
2022-07-05 11:08:03 +0300

    user service updated

---
+++ post-microservice/src/main/java/com/iknow/postmicroservice/service/UserService.java
@@ -0,0 +1,44 @@
+package com.iknow.postmicroservice.service;
+
+
+import com.iknow.postmicroservice.model.LikeModel;
+import com.iknow.postmicroservice.model.User;
+import com.iknow.postmicroservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new RuntimeException("user not found!"));
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findUserById(id)
+                .orElseThrow(() -> new RuntimeException("user not found!"));
+    }
+
+    protected boolean isUserContains(final List<LikeModel> list, final String username) {
+
+        log.info("search: " + username);
+
+        var result = list.stream().anyMatch(o -> o.getUser().getUsername().equals(username));
+        log.info("result: " + result);
+
+        return result;
+    }
+
+
+}
