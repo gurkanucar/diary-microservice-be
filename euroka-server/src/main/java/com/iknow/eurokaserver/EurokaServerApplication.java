b57338d47bf20aef53f153b8e06fbcfbc9a67847
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-08 11:41:17 +0300

    eureka server created

---
+++ euroka-server/src/main/java/com/iknow/eurokaserver/EurokaServerApplication.java
@@ -0,0 +1,15 @@
+package com.iknow.eurokaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurokaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurokaServerApplication.class, args);
+    }
+
+}
---
+++ euroka-server/src/test/java/com/iknow/eurokaserver/EurokaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iknow.eurokaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurokaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
