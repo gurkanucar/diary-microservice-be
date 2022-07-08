9b177625408f22f811c31d735f06aed2909e3af5
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-08 11:15:25 +0300

    userRequestService feign client value updated

--- api-gateway/src/main/java/com/iknow/apigateway/requestService/UserRequestService.java
+++ api-gateway/src/main/java/com/iknow/apigateway/requestService/UserRequestService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(value = "course-service",
+@FeignClient(value = "user-microservice",
         path = "/user",
         //url = "http://localhost:8081",
         url = "${user-microservice.url}",
