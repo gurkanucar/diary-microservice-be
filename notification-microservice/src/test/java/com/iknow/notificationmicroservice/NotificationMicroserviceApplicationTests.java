40ecec98cfd77702330c1299d52213e7fdb90927
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-13 18:47:53 +0300

    notification microservice created

---
+++ notification-microservice/src/test/java/com/iknow/notificationmicroservice/NotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iknow.notificationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
