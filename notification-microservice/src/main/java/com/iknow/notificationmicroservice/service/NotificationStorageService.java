ff9f9a8460dd8ff317c249563f432f37ea72af51
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-14 18:11:31 +0300

    notification services created

---
+++ notification-microservice/src/main/java/com/iknow/notificationmicroservice/service/NotificationStorageService.java
@@ -0,0 +1,48 @@
+package com.iknow.notificationmicroservice.service;
+
+import com.iknow.notificationmicroservice.model.Notification;
+import com.iknow.notificationmicroservice.repository.NotificationStorageRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class NotificationStorageService {
+
+    private final NotificationStorageRepository notifRepository;
+
+    public NotificationStorageService(NotificationStorageRepository notifRepository) {
+        this.notifRepository = notifRepository;
+    }
+
+    public Notification createNotificationStorage(Notification notificationStorage) {
+        return notifRepository.save(notificationStorage);
+    }
+
+
+    public Notification getNotificationsByID(String id) {
+        return notifRepository.findById(id).orElseThrow(() -> new RuntimeException("notification not found!"));
+    }
+
+    public List<Notification> getNotificationsByUserIDNotRead(String userID) {
+        return notifRepository.findByUserToIdAndDeliveredFalse(userID);
+    }
+
+
+    public List<Notification> getNotificationsByUserID(String userID) {
+        return notifRepository.findByUserToId(userID);
+    }
+
+    public Notification changeNotifStatusToRead(String notifID) {
+        var notif = notifRepository.findById(notifID)
+                .orElseThrow(() -> new RuntimeException("not found!"));
+        notif.setDelivered(true);
+        return notifRepository.save(notif);
+    }
+
+    public void clear() {
+        notifRepository.deleteAll();
+    }
+}
