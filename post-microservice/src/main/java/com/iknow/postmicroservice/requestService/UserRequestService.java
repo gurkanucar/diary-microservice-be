96a29fde330205d3711df1b023932b27520b1293
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-14 09:22:29 +0300

    Feign Client value changed for UserRequestService

--- post-microservice/src/main/java/com/iknow/postmicroservice/requestService/UserRequestService.java
+++ post-microservice/src/main/java/com/iknow/postmicroservice/requestService/UserRequestService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(value = "user-microservice",
+@FeignClient(value = "api-gateway",
         path = "/user",
         //url = "http://localhost:8081",
         //  url = "${user-microservice.url}",
