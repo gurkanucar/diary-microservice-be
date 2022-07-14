a7aa1debf69c02697072fd1d77735815a2049a36
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-14 16:45:50 +0300

    Revert "Feign Client value changed for UserRequestService"
    This reverts commit 96a29fde330205d3711df1b023932b27520b1293.

--- post-microservice/src/main/java/com/iknow/postmicroservice/requestService/UserRequestService.java
+++ post-microservice/src/main/java/com/iknow/postmicroservice/requestService/UserRequestService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(value = "api-gateway",
+@FeignClient(value = "user-microservice",
         path = "/user",
         //url = "http://localhost:8081",
         //  url = "${user-microservice.url}",
