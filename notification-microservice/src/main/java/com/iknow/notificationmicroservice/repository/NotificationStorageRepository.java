769125b1b06925945cb02dd00f2829046e7adebe
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-14 18:11:19 +0300

    notification repository created

---
+++ notification-microservice/src/main/java/com/iknow/notificationmicroservice/repository/NotificationStorageRepository.java
@@ -0,0 +1,21 @@
+package com.iknow.notificationmicroservice.repository;
+
+import com.iknow.notificationmicroservice.model.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface NotificationStorageRepository extends MongoRepository<Notification, String> {
+
+    Optional<Notification> findById(String id);
+
+    List<Notification> findByUserToId(String id);
+
+
+    List<Notification> findByUserToIdAndDeliveredFalse(String id);
+
+
+}
