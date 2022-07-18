f69d8c71a1637c87a7bd848d9dcdb59585bfdac1
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-18 13:31:49 +0300

    notification feign client updated

--- api-gateway/src/main/java/com/iknow/apigateway/requestService/NotificationService.java
+++ api-gateway/src/main/java/com/iknow/apigateway/requestService/NotificationService.java
@@ -2,22 +2,30 @@ package com.iknow.apigateway.requestService;
 
 import com.iknow.apigateway.model.Notification;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.ServerSentEvent;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
 
 @FeignClient(value = "notification-microservice",
-        path = "/notification",
+        path = "",
         configuration = FeignConfiguration.class)
 public interface NotificationService {
 
 
+    @GetMapping("/push-notifications/{userID}")
+    Flux<ServerSentEvent<List<Notification>>> streamLastMessage(@PathVariable String userID);
 
-    @GetMapping("/{userID}")
-    public Flux<ServerSentEvent<List<Notification>>> streamLastMessage(@PathVariable String userID);
 
+    @GetMapping("/notification/{userID}")
+    ResponseEntity<List<Notification>> getNotificationsByUserID(@PathVariable String userID);
+
+    @PostMapping("/notification")
+    ResponseEntity<Notification> getNotificationsByUserID(@RequestBody Notification notification);
+
+    @PatchMapping("/notification/read/{notifID}")
+    ResponseEntity<Notification> changeNotifStatusToRead(@PathVariable String notifID);
 
 }
