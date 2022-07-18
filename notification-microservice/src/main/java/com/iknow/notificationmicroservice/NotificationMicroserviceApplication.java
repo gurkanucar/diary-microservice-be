aa9f3be740da6c74503b81fcffc9195eb1172bf0
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-18 11:14:58 +0300

    mongo annotation added to main app

--- notification-microservice/src/main/java/com/iknow/notificationmicroservice/NotificationMicroserviceApplication.java
+++ notification-microservice/src/main/java/com/iknow/notificationmicroservice/NotificationMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.iknow.notificationmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class NotificationMicroserviceApplication {
 
 	public static void main(String[] args) {
